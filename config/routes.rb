eea183e305644ab280dfab2dd20a0d39ba8c51d3
davegregg <davegregg@gmail.com>
2017-04-10 06:02:48 -0400

    User auth. Signup mailers. Routes tweaks. User controller refactoring.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
   scope '/api' do
-    resources :users
+    resources :users, except: [:index]
+    post '/users/authentication', to: 'users#authenticate'
   end
+
+  ## Sara's routes
+  # Profile
+  # SignUp
+  # SignIn
+  # OnBoarding
+
+  # rails generate resource Prescription
 
   # root to: redirect(
   #   Rails.env.production? ? 'https://.../' : 'http://localhost:8000/'
