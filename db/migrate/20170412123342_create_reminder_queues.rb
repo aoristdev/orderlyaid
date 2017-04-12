cac8374475f9d3f1e0f32cccc2e4098382ef3843
davegregg <davegregg@gmail.com>
2017-04-12 08:38:34 -0400

    set datatype on transmit_time

--- db/migrate/20170412123342_create_reminder_queues.rb
+++ db/migrate/20170412123342_create_reminder_queues.rb
@@ -2,7 +2,7 @@ class CreateReminderQueues < ActiveRecord::Migration[5.0]
   def change
     create_table :reminder_queues do |t|
       t.references :prescriptions, foreign_key: true
-      t.string :transmit_time
+      t.time :transmit_time
 
       t.timestamps
     end
