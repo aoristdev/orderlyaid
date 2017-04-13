120bcdf5c72a0bc02b8eeb494a563f20225571b1
davegregg <davegregg@gmail.com>
2017-04-13 16:00:07 -0400

    Preparing for reminder creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,26 @@
 Rails.application.routes.draw do
 
-  resources :prescriptions
+  # Prefix        Verb   URI Pattern                   Controller#Action
+  # prescriptions GET    /prescriptions(.:format)      prescriptions#index
+  #               POST   /prescriptions(.:format)      prescriptions#create
+  #  prescription GET    /prescriptions/:id(.:format)  prescriptions#show
+  #               PATCH  /prescriptions/:id(.:format)  prescriptions#update
+  #               PUT    /prescriptions/:id(.:format)  prescriptions#update
+  #               DELETE /prescriptions/:id(.:format)  prescriptions#destroy
+
   scope '/users' do
     post  '/create',         to: 'users#create'
     post  '/authenticate',   to: 'users#authenticate'
     post  '/profile',        to: 'users#show'
     patch '/update',         to: 'users#update'
     post  '/delete',         to: 'users#destroy'
+  end
+
+  scope '/rx' do
+    post  '/create', to: 'prescriptions#create'
+    post  '/show',   to: 'prescriptions#show'
+    patch '/update', to: 'prescriptions#update'
+    post  '/delete', to: 'prescriptions#destroy'
   end
 
   root to: redirect('http://localhost:3000/')
@@ -21,7 +35,6 @@ Rails.application.routes.draw do
   #   Rails.env.production? ? 'https://.../' : 'http://localhost:8000/'
   # )
 
-  #TODO customize routes for Prescriptions
   #TODO validations!!
 
 end
