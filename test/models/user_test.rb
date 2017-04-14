d49de58d415ac1374b5572cea340292087d19c22
Michael Stashevsky <mstashev@gmail.com>
2017-04-14 15:20:16 -0400

    added MiniTest to GEMFILE

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_user_invalid_without_email
+    user = build(:user, email: "")
+    refute user.valid?
+  end
+
+  def test_user_email_is_unique
+    user = build(:user, email: "a@a.com")
+    user1 = build(:user, email: "a@a.com")
+    assert user.save
+    refute user1.save
+  end
+
+  def test_user_has_many_prescriptions
+    user = build(:user)
+    prescription = build(:perscription)
+    prescription1 = build(:perscription)
+    user.prescriptions << prescription
+    user.prescriptions << prescription1
+    assert user.prescriptions.count == 2
+  end
+
+  def test_user_has_many_reminders
+    user = build(:user)
+    reminder = build(:reminder)
+    reminder1 = build(:reminder)
+    user.reminders << reminder
+    user.reminders << reminder1
+    assert user.reminders.count == 2
+  end
+
 end
