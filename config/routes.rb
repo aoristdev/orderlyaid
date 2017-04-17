8add40df838bb7cb728b62978658761068b760d7
Sara Trempe <88.trempe.sara@gmail.com>
2017-04-17 15:55:58 -0400

    time picker

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,18 @@ Rails.application.routes.draw do
   #               DELETE /prescriptions/:id(.:format)  prescriptions#destroy
 
   scope '/users' do
-    post  '/create',         to: 'users#create'
-    post  '/authenticate',   to: 'users#authenticate'
-    post  '/profile',        to: 'users#show'
-    patch '/update',         to: 'users#update'
-    post  '/delete',         to: 'users#destroy'
+    post   '/create',         to: 'users#create'
+    post   '/authenticate',   to: 'users#authenticate'
+    get    '/profile',        to: 'users#show'
+    patch  '/update',         to: 'users#update'
+    delete '/delete',         to: 'users#destroy'
   end
 
   scope '/rx' do
-    post  '/create', to: 'prescriptions#create'
-    post  '/show',   to: 'prescriptions#show'
-    patch '/update', to: 'prescriptions#update'
-    post  '/delete', to: 'prescriptions#destroy'
+    post   '/create',  to: 'prescriptions#create'
+    get    '/show',    to: 'prescriptions#show'
+    patch  '/update',  to: 'prescriptions#update'
+    delete '/delete',  to: 'prescriptions#destroy'
   end
 
   root to: redirect('http://localhost:3000/')
