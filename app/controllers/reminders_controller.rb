1ddcc69e026433c682b5f69c7c1fa5dd7ac5006a
davegregg <davegregg@gmail.com>
2017-04-20 17:44:48 -0400

    Tidying up controllers and validations

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -2,6 +2,7 @@ class RemindersController < ApplicationController
 
   before_action :require_user
 
+  def show; end
   def history
     render json: Reminder.where('transmit_time < ?', DateTime.now)
   end
